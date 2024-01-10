97aa6867358d548508e6b14a9169e41f25f16f7a
jan Jak pijak <kowalalex204@gmail.com>
2024-01-10 19:49:04 +0800

    Добавьте файлы проекта.

---
+++ Faktorial/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("найти факториал выбраного пользователем числа");
+            Console.WriteLine("введите число");
+            ulong number = ulong.Parse(Console.ReadLine());
+            try
+            {
+                ulong fibonacci = FactRecursive(number) * 1;
+                Console.WriteLine($"Fibonacci = {fibonacci};");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            Console.ReadKey();
+        }
+        private static uint Fact(uint number)
+        {
+            uint fact = 1;
+            for (uint i = 1; i <= number; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+
+        private static ulong FactRecursive(ulong number) =>
+            number == 0 ? 1 : checked(FactRecursive(number - 1) * number);
+
+        private static uint Fibonacci(uint number)
+        {
+            uint f0 = 0;
+            uint f1 = 1;
+
+            uint fn = 0;
+            for (uint i = 2; i <= number; i++)
+            {
+                fn = f1 + f0;
+                f0 = f1;
+                f1 = fn;
+            }
+            return fn;
+        }
+
+        private static uint FibonacciRecursive(uint number)
+        {
+            if (number <= 2)
+            {
+                return 1;
+            }
+            return FibonacciRecursive(number - 1) + FibonacciRecursive(number - 2);
+        }
+
+        private static uint Recurus(uint number)
+        {
+            Console.WriteLine("Введите k, к<3: ");
+            int k = int.Parse(Console.ReadLine());
+
+            double multiplication = 1;
+            int j = 0;
+
+            if (j > k)
+            {
+                return 1;
+            }
+
+            for (j = -4; j <= k; j++)
+            {
+                double buf = Math.Pow(k, j + 2) * j / ((double)j - 3);
+                if (buf != 0 && !double.IsInfinity(buf))
+                {
+                    multiplication *= buf;
+                }
+            }
+            return (uint)multiplication;  
+        }
+    }
+}
+
---
+++ Индивидуальная задача РАЗДЕЛ 3.2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Индивидуальная_задача_РАЗДЕЛ_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Дано натуральное число n (n<9999) и число m. Найти сумму m-последних цифр числа n.");
+            //Console.WriteLine("введите число n (n<9999)");
+            //uint n = uint.Parse(Console.ReadLine());
+            //if (n > 9999)
+            //{
+            //    Console.WriteLine("error\a");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("введите число m");
+            //    uint m = uint.Parse(Console.ReadLine());
+            //    if (m>=4)
+            //    {
+            //        Console.WriteLine("error\a");
+            //    }
+
+            //    uint sumDigits = 0;
+            //    uint buf = n;
+
+            //    for (int i = 0; i < m; i++)
+            //    {
+            //        uint lastDigit = buf % 10;  
+            //        sumDigits += lastDigit;
+            //        buf /= 10;
+            //    }
+
+            //    Console.WriteLine($"сумма {m}-последних цифр числа {n} {sumDigits}");
+            //}
+            //Console.ReadKey();
+
+            Console.WriteLine("введите число");
+            int number =int.Parse(Console.ReadLine());
+            int lastDigit;
+            int delimeter = 10;
+            int result=0;
+            int buf = number;
+            while ((lastDigit = buf % delimeter) != 0)
+            {
+                result *= delimeter;
+                result += lastDigit;
+                buf /= 10;
+            }
+            Console.WriteLine($"source number = {number}; reverse number = {result}");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
+
